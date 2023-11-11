40653d8c5b33d1bd6fa7dab15ffe546a62257d5d
maurice-cubicit <maurice@cubicit.co.za>
2023-11-11 13:13:10 +0200

    Added menu button

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,34 +1,38 @@
 import { Link } from "@solidjs/router";
 import { Component } from "solid-js";
 import Logo from "../assets/images/logo.png"
+import { IoMenuSharp } from 'solid-icons/io'
 
 const Nav:Component = () => {
     return (
         <nav class="w-full py-2 sticky top-0" style={{"background-color":'#EEDFAF'}}>
-            <div class="w-11/12 md:w-10/12 m-auto flex gap-96 items-center">
+            <div class="w-11/12 md:w-10/12 m-auto flex md:gap-96 items-center justify-between md:justify-start">
                 <div>
-                    <Link href="/">
+                    <Link href="#">
                     <img src={Logo} class="h-10"/>
                     </Link>
                 </div>
                 <div class="hidden md:block">
                    <ul class="flex gap-10">
-                    <Link href="/">
+                    <Link href="#">
                         <li>Home</li>
                     </Link>
                         <li>|</li>
-                    <Link href="/services">
+                    <Link href="#">
                         <li>Services</li>
                     </Link>
                         <li>|</li>
-                    <Link href="/products">
+                    <Link href="#">
                         <li>Products</li>
                     </Link>
                         <li>|</li>
-                    <Link href="/contact">
+                    <Link href="#">
                         <li>Contact Us</li>
                     </Link>
                    </ul>
+                </div>
+                <div class="block md:hidden">
+                    <IoMenuSharp class="text-2xl"/>
                 </div>
             </div>
         </nav>
