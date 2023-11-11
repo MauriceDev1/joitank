de4d48331e91f8d7b2c7903d264ac4bd71d189eb
maurice-cubicit <maurice@cubicit.co.za>
2023-11-11 12:40:57 +0200

    fix contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,31 +4,35 @@ const Contact:Component = () => {
     return (
         <div class="w-full h-screen py-16" style={{"background-color":'#EEDFAF'}}>
             <div class="w-11/12 md:w-10/12 m-auto flex flex-wrap">
-                <div class="w-1/2">
+                <div class="w-1/2 flex flex-col gap-3">
                     <label>Name and Surname</label>
                     <input 
                         type="text" 
-                        class="w-full h-10 rounded border border-gray-300"
+                        name="names"
+                        placeholder="Enter name and surname"
+                        class="w-full h-10 rounded border border-gray-300 px-2"
                     />
                     <label>Address of Installation</label>
                     <textarea 
-                        name="address" 
+                        name="address"
                         id="" 
                         cols="30" 
                         rows="4"
                         placeholder="Enter address here"
-                        class="w-full resize-none rounded border border-gray-300"
+                        class="w-full resize-none rounded border border-gray-300 p-2"
                     >
                     </textarea>
                     <label>Email Address</label>
                     <input 
-                        type="text" 
-                        class="w-full h-10 rounded border border-gray-300"
+                        type="email" 
+                        placeholder="Enter email address"
+                        class="w-full h-10 rounded border border-gray-300 px-2"
                     />
                     <label>Contact Number</label>
                     <input 
-                        type="tell" 
-                        class="w-full h-10 rounded border border-gray-300"
+                        type="tel"
+                        placeholder="Enter telephone number" 
+                        class="w-full h-10 rounded border border-gray-300 px-2"
                     />
                     <label>Which Tank do you need?</label>
                     <div class="w-full flex flex-wrap">
@@ -57,7 +61,7 @@ const Contact:Component = () => {
                             <p>10000L</p>
                         </div>
                     </div>
-                    <button class="bg-black text-white h-10 rounded w-full">Submit</button>
+                    <button class="bg-black text-white h-10 rounded w-full mt-4">Submit</button>
                 </div>
                 <div class="w-1/2">
                     <div class="p-5">
@@ -71,6 +75,8 @@ const Contact:Component = () => {
                                 Construction<br></br>
                                 Training  
                             </p>
+                            <h1 class="text-2xl py-3 text-center font-semibold">Get intouch</h1>
+                            <p></p>
                         </div>
                     </div>
                 </div>
