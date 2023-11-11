8119cfc62cc5e6d73e989093599aced96921128e
maurice-cubicit <maurice@cubicit.co.za>
2023-11-11 09:41:27 +0200

    clean app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { Route, Routes } from "@solidjs/router"
 import Home from "./pages/Home"
-import Services from "./pages/Services"
-import Products from "./pages/Products"
-import Contact from "./pages/Contact"
 
 
 function App() {
