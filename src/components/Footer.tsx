70edcdf66613b5cf5d6fdda34cda7c0d8ae40349
maurice-cubicit <maurice@cubicit.co.za>
2023-11-28 07:02:19 +0200

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Component } from "solid-js";
 const Footer: Component = () => {
     return (
         <footer class="py-3 bg-red-500">
-            Test
+            
         </footer>
     )
 }
