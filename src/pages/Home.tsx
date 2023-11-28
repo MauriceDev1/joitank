2bc026bb013f7c787bf02c8b98a7a0efdae79991
maurice-cubicit <maurice@cubicit.co.za>
2023-11-28 07:07:06 +0200

    Remove errors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Component } from "solid-js";
 import Hero from "../components/Hero";
-import Hero2 from "../components/Hero2";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import About from "../components/About";
